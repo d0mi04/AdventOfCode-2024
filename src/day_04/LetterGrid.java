0acff873a9e9921e68b07f5edfbfd7dd6ee6979e
dominika <d0mi04@githum.com>
2024-12-05 22:32:21 +0100

    [Advent of code 2024] day 04, part 1

---
+++ src/day_04/LetterGrid.java
@@ -0,0 +1,27 @@
+package day_04;
+
+import java.util.ArrayList;
+
+public class LetterGrid {
+    ArrayList<String> lines;
+
+    public LetterGrid(ArrayList<String> lines) {
+        this.lines = lines;
+    }
+
+    public int getRows() {
+        return lines.size();
+    }
+
+    public int getColumns() {
+        return lines.getFirst().length();
+    }
+
+    public char getCharAt(int row, int column) {
+        return lines.get(row).charAt(column);
+    }
+
+    public ArrayList<String> getLines() {
+        return lines;
+    }
+}
